07790d750b227a20305ae59d0233027b969fb261
Bryan Martins <bryanmaraujo544@gmail.com>
2022-06-21 13:49:47 -0300

    test branch

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('🔷 Trainya App 🔷');
+  res.send('🔷 Trainya App');
 });
 
 app.listen(PORT, () => console.log('🔥 Server Running! 🔥'));
