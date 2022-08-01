ae949e38556db38f664bb50e1881d5387046321e
Diego Arruda <arrudadiego45@gmail.com>
2022-08-01 02:00:15 -0300

    refactor(src): add new folders and tests in __tests__
    Add folders to repositories in memory and integration. Create an test to compare a sum to a result

---
+++ __tests__/units/CreateProductCategory.spec.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+describe('Crete product category', () => {
+  it('should be 4', () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+});
---
+++ src/app/repositories/in-memory/products-categories/ProductsCategoriesRepositoryInMemory.ts
@@ -0,0 +1,3 @@
+class ProductsCategoriesRepositoryInMemory implements ProductsCategoriesRepository {
+
+}
