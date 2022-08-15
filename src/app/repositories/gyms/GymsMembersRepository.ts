3c9b6acd268e8c8d8c78028d670651b8bb8fad15
Diego Arruda <arrudadiego45@gmail.com>
2022-08-15 08:27:51 -0300

    feat(gymmember): add method to create gymMembers

--- src/app/repositories/gyms/GymsMembersRepository.ts
+++ src/app/repositories/gyms/GymsMembersRepository.ts
@@ -1,11 +1,27 @@
 import { PrismaClient } from '@prisma/client';
 const { gymMember } = new PrismaClient();
 
+interface IGymMember {
+  gym_id: number;
+  member_id: number;
+}
+
 class GymsMembersRepository {
   async findAll() {
     const gymMembers = await gymMember.findMany();
 
     return gymMembers;
+  }
+
+  async create({ gym_id, member_id }: IGymMember) {
+    const createdGymMember = await gymMember.create({
+      data: {
+        gym_id,
+        member_id,
+      },
+    });
+
+    return createdGymMember;
   }
 }
 
--- src/routes/GymMembers.routes.ts
+++ src/routes/GymMembers.routes.ts
@@ -3,5 +3,6 @@ import GymsMembersController from '../app/controllers/gyms/GymsMembersController
 const gymMembersRoutes = Router();
 
 gymMembersRoutes.get('/gymMembers', GymsMembersController.index);
+gymMembersRoutes.post('/gymMembers', GymsMembersController.store);
 
 export default gymMembersRoutes;
