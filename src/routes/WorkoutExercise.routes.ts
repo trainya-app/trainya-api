d35bd9f2bdfa1e511801909ce2822ebabecc14c4
Diego Arruda <arrudadiego45@gmail.com>
2022-08-21 21:55:50 -0300

    feat(workoutexercises): list all workoutExercises

---
+++ src/routes/WorkoutExercise.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import WorkoutsExercisesController from '../app/controllers/workouts/WorkoutsExercisesController';
+const workoutExercisesRoutes = Router();
+
+workoutExercisesRoutes.get(
+  '/workoutExercises',
+  WorkoutsExercisesController.index
+);
+// workoutExercisesRoutes.post(
+//   '/workoutExercises',
+//   WorkoutsExercisesController.store
+// );
+// workoutExercisesRoutes.delete(
+//   '/workoutExercises/:id',
+//   WorkoutsExercisesController.delete
+// );
+// workoutExercisesRoutes.get(
+//   '/workoutExercises/:id',
+//   WorkoutsExercisesController.show
+// );
+// workoutExercisesRoutes.put(
+//   '/workoutExercises/:id',
+//   WorkoutsExercisesController.update
+// );
+
+export default workoutExercisesRoutes;
