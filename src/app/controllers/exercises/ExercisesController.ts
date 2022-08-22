1a47c5264021dc52950c9dd7a7c9f127ae1530f3
Diego Arruda <arrudadiego45@gmail.com>
2022-08-21 21:26:11 -0300

    feat(exercises): create exercises

--- src/app/controllers/exercises/ExercisesController.ts
+++ src/app/controllers/exercises/ExercisesController.ts
@@ -1,10 +1,38 @@
 import { Request, Response } from 'express';
+import { isSomeEmpty } from '../../../utils/isSomeEmpty';
 import ExercisesRepository from '../../repositories/exercises/ExercisesRepository';
 
 class ExercisesController {
   async index(req: Request, res: Response) {
     const exercises = await ExercisesRepository.findAll();
     return res.send({ exercises });
+  }
+
+  async store(req: Request, res: Response) {
+    const { name, comment, needsEquipment } = req.body;
+
+    const someFieldIsEmpty = isSomeEmpty([name, comment, needsEquipment]);
+    if (someFieldIsEmpty) {
+      return res.status(400).json({
+        message: 'Campos obrigatórios não foram enviados',
+        exercise: null,
+      });
+    }
+
+    const nameExists = await ExercisesRepository.findByName(name);
+    if (nameExists) {
+      return res.status(400).json({
+        message: 'Exercício já cadastrado',
+        exercise: null,
+      });
+    }
+
+    const exercise = await ExercisesRepository.store({
+      name,
+      comment,
+      needs_equipment: needsEquipment,
+    });
+    return res.status(200).json({ message: 'Exercício cadastrado!', exercise });
   }
 }
 
--- src/app/repositories/exercises/ExercisesRepository.ts
+++ src/app/repositories/exercises/ExercisesRepository.ts
@@ -1,9 +1,36 @@
 import { PrismaClient } from '@prisma/client';
 const { exercise } = new PrismaClient();
+
+interface IExercises {
+  name: string;
+  comment: string;
+  needs_equipment: boolean;
+}
+
 class ExercisesRepository {
   async findAll() {
     const exercises = await exercise.findMany();
     return exercises;
+  }
+
+  async findByName(name: string) {
+    const nameExists = await exercise.findFirst({
+      where: {
+        name,
+      },
+    });
+    return nameExists;
+  }
+
+  async store({ name, comment, needs_equipment }: IExercises) {
+    const createdExercise = await exercise.create({
+      data: {
+        name,
+        comment,
+        needs_equipment,
+      },
+    });
+    return createdExercise;
   }
 }
 
