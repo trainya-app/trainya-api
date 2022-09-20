b08787dc49ff3bfd82243f18f59eea96dd381209
Diego Arruda <arrudadiego45@gmail.com>
2022-09-19 21:02:28 -0300

    Add multer to routes with files to be uploaded

--- src/routes/Workouts.routes.ts
+++ src/routes/Workouts.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import WorkoutsController from '../app/controllers/workouts/WorkoutsController';
 const workoutRoutes = Router();
+import MulterMiddleware from '../app/middlewares/MulterMiddleware';
 
 workoutRoutes.get('/workouts', WorkoutsController.index);
-workoutRoutes.post('/workouts', WorkoutsController.store);
+workoutRoutes.post(
+  '/workouts',
+  MulterMiddleware.any(),
+  WorkoutsController.store
+);
 workoutRoutes.delete('/workouts/:id', WorkoutsController.delete);
 workoutRoutes.get('/workouts/:id', WorkoutsController.show);
 workoutRoutes.put('/workouts/:id', WorkoutsController.update);
